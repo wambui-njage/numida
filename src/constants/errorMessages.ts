54e6166b8b6a1eafb5ec2ac5331126372c0fa134
wambui-njage <wambui.njage@gmail.com>
2024-08-27 15:21:17 +0300

    feature : contrants error and success

---
+++ src/constants/errorMessages.ts
@@ -0,0 +1,9 @@
+const errorMessages = {
+  networkError:
+    "A network error occurred. Please check your connection and try again.",
+  noLoanProducts: "No loan products available",
+  noLoanProductSelected: "Please select a loan product before applying.",
+  genericError: "Something went wrong",
+};
+
+export default errorMessages;
---
+++ src/constants/successMessages.ts
@@ -0,0 +1,6 @@
+const successMessages = {
+  backOnline: "You are back online 👍🏾!",
+  loanApplication: "Loan application submitted successfully!",
+};
+
+export default successMessages;
