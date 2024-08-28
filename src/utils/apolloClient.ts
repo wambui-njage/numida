3af7da785d9692ff479b9b0150a026a895b0d466
wambui-njage <wambui.njage@gmail.com>
2024-08-28 09:46:37 +0300

    bugfix:handle network error

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,10 +1,36 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { API_URL } from "@env";
 
+console.log(`${API_URL}/graphql`);
+
+// Custom fetch function with timeout (Network error was taking more than 60sec to timeout)
+const fetchWithTimeout = (
+  url: RequestInfo | URL,
+  options: RequestInit | undefined,
+  timeout = 6000
+): Promise<Response> => {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject(new Error("Network request timed out"));
+    }, timeout);
+
+    fetch(url, options)
+      .then((response) => {
+        clearTimeout(timeoutId);
+        resolve(response);
+      })
+      .catch((error) => {
+        clearTimeout(timeoutId);
+        reject(error);
+      });
+  });
+};
+
 // Create Apollo Client
 const client = new ApolloClient({
   link: new HttpLink({
     uri: `${API_URL}/graphql`,
+    fetch: fetchWithTimeout,
   }),
   cache: new InMemoryCache(),
 });
