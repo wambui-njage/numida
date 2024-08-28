de3e9b17a630be168f74910fd2876f1b5ff296a2
wambui-njage <wambui.njage@gmail.com>
2024-08-28 10:44:41 +0300

    feature: jest configaration

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: "jest-expo",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  testMatch: ["**/*.test.ts", "**/*.test.tsx"], // Patterns to find test files
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  collectCoverage: false,
+};
